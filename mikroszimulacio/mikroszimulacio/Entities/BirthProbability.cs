b4f03a142ffc5c2aac224382a89181f88c3e3af0
Krisi <kristof.sarkozi@stud.uni-corvinus.hu>
2022-12-02 10:18:04 +0100

    first

---
+++ mikroszimulacio/mikroszimulacio/Entities/BirthProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikroszimulacio.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+
+        public int Child_count { get; set; }
+
+        public int Probability { get; set; }
+    }
+}
